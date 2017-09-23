fcb12b45a2632f2937c3bf03e400269ca43b400d
Konatsup <konatsukomatsu@gmail.com>
2017-09-23 16:22:33 +0900

    firstcommit

---
+++ Assets/Script/Monster1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster1 : MonoBehaviour {
+
+
+	public float speed = 0.1f;
+	private float nextPosX;
+	// Use this for initialization
+	void Start () {
+		nextPosX = this.transform.position.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nextPosX += speed * -1.0f;
+		gameObject.transform.position = new Vector3(nextPosX,0f,0f);
+		
+	}
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		if (coll.gameObject.tag == "Monster2") {
+			Debug.Log ("sdkl;f:]sdskjfl;s:fskl");
+		}
+	}
+		
+}
